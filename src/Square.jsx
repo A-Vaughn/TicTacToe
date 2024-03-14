9a44526d33c1c2720c73a4467b05012a0fe6cbd0
A-Vaughn <akilvaughn14@gmail.com>
2024-03-14 15:07:31 -0400

    first commit

---
+++ src/Square.jsx
@@ -0,0 +1,23 @@
+import Circle from "./Circle"
+import Cross from "./Cross"
+
+const Square = ({position, value, take_turn})=>{
+    const checkTurn = ()=>{
+        if (value == "EMPTY")
+            take_turn(position)
+        
+           
+        
+    }
+    return (
+        <div className = "Square" onClick={checkTurn}>
+            {/* square {position} */}
+            {/* <Circle/> */}
+            {/* <Cross/> */}
+            {value == "CIRCLE" && <Circle/>}
+            {value == "CROSS" && <Cross/>}
+        </div>
+    )
+}
+
+export default Square
